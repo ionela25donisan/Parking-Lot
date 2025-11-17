55174654371ff83934f4f0731834b17daa3daa50
Ionela <ionelateodora.donisan@ulbsibiu.ro>
2025-11-17 23:35:45 +0200

    "Created the entities"

--- src/main/java/org/example/parkinglot/entities/User.java
+++ src/main/java/org/example/parkinglot/entities/User.java
@@ -1,26 +1,31 @@
 package org.example.parkinglot.entities;
 
 import jakarta.persistence.*;
+import java.util.Set;
+import java.util.LinkedHashSet;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users") // opțional, dar păstrat pentru claritate
 public class User {
-    @Column(name = "username")
-    private String username;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    public String getUsername() {
-        return username;
-    }
+    @Column(name = "username")
+    private String username;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    @Column(name = "email")
+    private String email;
 
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "owner", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<Car> cars = new LinkedHashSet<>();
+
+    // Getter și Setter pentru id
     public Long getId() {
         return id;
     }
@@ -29,4 +34,39 @@ public class User {
         this.id = id;
     }
 
+    // Getter și Setter pentru username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter și Setter pentru email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter și Setter pentru password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter și Setter pentru cars
+    public Set<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars = cars;
+    }
 }
